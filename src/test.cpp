2d4000167f797ea6aa7790a781255ed93f573fea
hoshi524 <hoshi524@gmail.com>
2018-01-31 06:49:06 +0900

    update

--- src/test.cpp
+++ src/test.cpp
@@ -2,30 +2,75 @@
 #include <sys/time.h>
 using namespace std;
 
+inline unsigned get_random() {
+  static unsigned y = 2463534242;
+  return y ^= (y ^= (y ^= y << 13) >> 17) << 5;
+}
+
+constexpr int N = 1 << 8;
+
+int n, k, x[N], r[N];
+bool m[N][N];
+float pi;
+
+auto dist(int a, int b) {
+  if (a > b) swap(a, b);
+  return sin((b - a) * pi);
+}
+
+auto calc(int t) {
+  int c = 0;
+  float v = 0;
+  for (int i = 0; i < k; ++i) {
+    if (m[t][x[i]]) {
+      ++c;
+      v += dist(k, i);
+    }
+  }
+  int z = c * (1 << 24) - v * (1 << 16) + (get_random() & 0xff);
+  return z;
+}
+
 class PointsOnTheCircle {
  public:
   vector<int> permute(vector<int> matrix) {
-    int N = (int)sqrt(matrix.size());
-    vector<int> ret(N);
-    for (int i = 0; i < N; ++i) {
-      ret[i] = N - i - 1;
+    {  // init
+      n = (int)sqrt(matrix.size());
+      pi = M_PI / n;
+      for (int i = 0; i < n; ++i) {
+        r[i] = i;
+        for (int j = 0; j < n; ++j) m[i][j] = matrix[i * n + j];
+      }
     }
-    return ret;
+    {  // solve
+      for (k = 0; k < n; ++k) {
+        int a = 0, v = INT_MIN;
+        for (int i = 0; i + k < n; ++i) {
+          int t = calc(r[i]);
+          if (v < t) {
+            v = t;
+            a = i;
+          }
+        }
+        x[k] = r[a];
+        r[a] = r[n - k - 1];
+      }
+    }
+    {  // output
+      vector<int> ret(n);
+      for (int i = 0; i < n; ++i) ret[i] = x[i];
+      return ret;
+    }
   }
 };
 
 // -------8<------- end of solution submitted to the website -------8<-------
-template <class T>
-void getVector(vector<T>& v) {
-  for (int i = 0; i < v.size(); ++i) cin >> v[i];
-}
-
 int main() {
   PointsOnTheCircle pc;
   int M;
   cin >> M;
   vector<int> matrix(M);
-  getVector(matrix);
+  for (int i = 0; i < M; ++i) cin >> matrix[i];
 
   vector<int> ret = pc.permute(matrix);
   cout << ret.size() << endl;
